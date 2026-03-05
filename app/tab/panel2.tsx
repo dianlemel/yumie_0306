51248f3820f1611af12fcb61adde3d5f21fb1b14
Dian <dianlemel@gmail.com>
2026-03-05 19:59:13 +0800

    13

--- app/tab/panel2.tsx
+++ app/tab/panel2.tsx
@@ -17,7 +17,7 @@ const charactersList = [
                     icon={<BsStar size={15}/>}>
                     <List.Item>大量印刷之微小偏移及細小刷點、擦點不屬於瑕疵，完美主義者請勿下單。</List.Item>
                     <List.Item>若商品因使用者不當拆卸或使用造成故障、磨損、髒污或破損等情況，恕不接受退貨。</List.Item>
-                    <List.Item>若包裹未取被退件，如：錯誤的姓名/地址、物流送達時無人收件等，經查詢非為箱箱出貨失誤的情形，須事先酌收運費方可再寄出。</List.Item>
+                    <List.Item>若包裹未取被退件，如：錯誤的姓名/地址、物流送達時無人收件等，經查詢非為小幫手出貨所導致失誤的情形，須事先酌收運費方可再寄出。</List.Item>
                 </List>
                 <Text size="sm">※請注意：2.0募資之回饋紀念品於到貨次日起七天內需確認紀念品是否有瑕疵。</Text>
             </Stack>
@@ -43,7 +43,7 @@ const charactersList = [
         content: (
             <Stack align="stretch" justify="flex-start">
                 <Text
-                    size="sm">周邊開始製作條件為募資達標20萬後，會開始製作紀念週邊，外套為訂製款會有小幫手聯絡買家詢問尺寸等詳細資訊，周邊製作及準備至少需要3個月時間。</Text>
+                    size="sm">周邊開始製作條件為募資達標20萬後，會開始製作紀念週邊，周邊製作及準備至少需要3個月時間。</Text>
             </Stack>
         ),
     },
